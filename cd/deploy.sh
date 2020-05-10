cfc6ca21b617f75746564dc75f2a4c4544c08ba2
Glib Briia <glib.briia@gmail.com>
2020-05-10 20:37:21 +0100

    Update scripts

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew uploadArchives -Psigning.gnupg.executable=gpg -Psigning.gnupg.useLegacyGpg=true -Psigning.gnupg.keyName=$GPG_KEY_NAME -Psigning.gnupg.passphrase=$GPG_PASSPHRASE -Dhttp.proxyHost=163.172.93.183 -Dhttp.proxyPort=5836 -Dhttps.proxyHost=163.172.93.183 -Dhttps.proxyPort=5836
+    ./gradlew uploadArchives -Psigning.gnupg.executable=gpg -Psigning.gnupg.useLegacyGpg=true -Psigning.gnupg.keyName=$GPG_KEY_NAME -Psigning.gnupg.passphrase=$GPG_PASSPHRASE
 fi
